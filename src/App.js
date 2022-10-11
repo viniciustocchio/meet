34380047916717ba9fc28db53c7a4e5683a29253
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-10-11 14:49:12 +0200

    testing list of suggestions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import CitySearch from './CitySearch';
 import EventList from './EventList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CitySearch />
         <EventList />
       </div>
     );
