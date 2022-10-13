5071095c866ed95df13edcea5ff52a0982a51702
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-10-13 21:17:44 +0200

    added atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('778be2d81eb54cb38755161192ff1be8').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
