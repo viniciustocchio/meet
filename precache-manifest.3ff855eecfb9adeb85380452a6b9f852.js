a05827f8f086f2a4ac7a8c4ccb426ae770166839
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-10-13 21:08:22 +0200

    Updates

---
+++ precache-manifest.3ff855eecfb9adeb85380452a6b9f852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa659db24de0b91f369c",
+    "url": "/meet/static/css/main.94da1d90.chunk.css"
+  },
+  {
+    "revision": "fa659db24de0b91f369c",
+    "url": "/meet/static/js/main.ebc9b33e.chunk.js"
+  },
+  {
+    "revision": "10ed07c1e5836996bbae",
+    "url": "/meet/static/js/runtime~main.e8c2338e.js"
+  },
+  {
+    "revision": "fa5da2086ae4cd11da5c",
+    "url": "/meet/static/js/2.24cc2039.chunk.js"
+  },
+  {
+    "revision": "c95c4531592ba9ba2cab",
+    "url": "/meet/static/js/3.8f8517ab.chunk.js"
+  },
+  {
+    "revision": "48db8f6ab26f89d5499211088f533e19",
+    "url": "/meet/index.html"
+  }
+];
