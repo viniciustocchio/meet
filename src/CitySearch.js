c357af97f612b0149e1800d56706056f8fc00ea8
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-10-14 17:02:09 +0200

    added info alert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import "./App.css"
+import "./App.css";
+import { InfoAlert } from './Alert';
+
 class CitySearch extends Component {
   state = {
     query: '',
