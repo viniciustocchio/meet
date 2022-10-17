ba2291932900acf0ff8727d8a25d99327fb891c1
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-10-17 16:47:56 +0200

    added chart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import NumberOfEvents from './NumberOfEvents';
 import { mockData } from "./mock-data";
 import { extractLocations, getEvents } from './api';
 import './nprogress.css';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
+// import EventGenre from './EventGenre';
 
 class App extends Component {
   state = {
@@ -42,10 +46,57 @@ class App extends Component {
     <div className="App">
       <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
       <NumberOfEvents />
+
+      <h4>Events in each city</h4>
+
+        {/* <div className="data-vis-wrapper">
+          <EventGenre events={events} />
+        <ResponsiveContainer height={400} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+        </div> */}
+
+        <ResponsiveContainer height={400} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+      
       <EventList events={this.state.events}/>
     </div>
-  );
-}
+    );
+  }
+
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
 }
 
 export default App;
